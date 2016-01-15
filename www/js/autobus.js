79ad23ef7528e9fb84ba17ea0a9ae9aaeb9a2701
Sergi Orra <sergi.orra.genero@est.fib.upc.edu>
2016-01-15 18:51:06 +0100

    button to map

--- www/js/autobus.js
+++ www/js/autobus.js
@@ -1,8 +1,7 @@
-
 // Range beacons screen.
 ;(function(app)
 {
-
+ 
   var entra25 = true;
   var entra69 = true;
   var entra252 = true;
@@ -11,31 +10,34 @@
   var checkedH10 = false;
   var bus = "no ha funcionat";
   var bus2 = "no ha funcionat";
+ 
+ 
 
-
-
+ 
   app.startRangingBeacons = function()
   {
 
+   
+ 
     function onRange(beaconInfo)
     {
       displayBeconInfo(beaconInfo);
     }
-
+ 
     function onError(errorMessage)
     {
       console.log('Range error: ' + errorMessage);
     }
-
+ 
     function displayBeconInfo(beaconInfo)
     {
       // Clear beacon HTML items.
       $('#id-screen-range-beacons .style-item-list').empty();
-
+ 
       // Sort beacons by distance.
       beaconInfo.beacons.sort(function(beacon1, beacon2) {
         return beacon1.distance > beacon2.distance; });
-
+ 
       // Generate HTML for beacons.
       $.each(beaconInfo.beacons, function(key, beacon)
       {
@@ -43,7 +45,7 @@
         $('#id-screen-range-beacons .style-item-list').append(element);
       });
     };
-
+ 
     function createBeaconHTML(beacon)
     {
       var htm = '<label>'
@@ -62,10 +64,10 @@
           checked22 = true;
         }
         else checked22 = false;
-
+ 
       }
       var colorClasses = app.beaconColorStyle(beacon.color);
-      
+     
       /*var htm = '<div class="' + colorClasses + '">'
         + '<table><tr><td>Major</td><td>' + beacon.major
         + '</td></tr><tr><td>Minor</td><td>' + beacon.minor
@@ -84,7 +86,7 @@
       if (beacon.distance)
       {
         var dis = app.formatDistance(beacon.distance);
-
+ 
         if(dis < 5 && dis > 2 && entra25 && checkedH10){
           var msg = new SpeechSynthesisUtterance('El bus número '+ bus + ' está a menos de cinco metros. ');
           msg.lang = 'es-ES';
@@ -92,7 +94,7 @@
           entra25 = false;
           entra69 = true;
         }
-
+ 
         else if(dis > 6 && dis < 9 && entra69 && checkedH10){
           var msg = new SpeechSynthesisUtterance('El bus número '+ bus + ' está a menos de nueve metros.');
           msg.lang = 'es-ES';
@@ -100,7 +102,7 @@
           entra69 = false;
           entra25 = true;
         }
-
+ 
         if(dis < 5 && dis > 2 && entra252 && checked22){
           var msg = new SpeechSynthesisUtterance('El bus número '+ bus2 + ' está a menos de cinco metros. ');
           msg.lang = 'es-ES';
@@ -108,7 +110,7 @@
           entra252 = false;
           entra692 = true;
         }
-
+ 
         else if(dis > 6 && dis < 9 && entra692 && checked22){
           var msg = new SpeechSynthesisUtterance('El bus número '+ bus2 + ' está a menos de nueve metros.');
           msg.lang = 'es-ES';
@@ -120,25 +122,25 @@
       htm += '</label></br>';
       return htm;
     };
-
+ 
     // Show screen.
     app.showScreen('id-screen-range-beacons');
     $('#id-screen-range-beacons .style-item-list').empty();
-
+ 
     // Request authorisation.
     estimote.beacons.requestAlwaysAuthorization();
-
+ 
     // Start ranging.
     estimote.beacons.startRangingBeaconsInRegion(
       {}, // Empty region matches all beacons.
       onRange,
       onError);
   };
-
+ 
   app.stopRangingBeacons = function()
   {
     estimote.beacons.stopRangingBeaconsInRegion({});
     app.showHomeScreen();
   };
-
+ 
 })(app);
